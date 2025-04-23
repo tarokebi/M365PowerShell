7d7cce9f5c5ef2c6b6f9930013ecb11432c56c6c
Taro KEBI <59550645+tarokebi@users.noreply.github.com>
2025-04-23 13:03:25 +0900

    Update UpdateCustomRbacManagementRole.ps1

--- UpdateCustomRbacManagementRole.ps1
+++ UpdateCustomRbacManagementRole.ps1
@@ -1,3 +1,14 @@
+# Check where "CustomMyDistributionGroups" belongs to
+Get-RoleAssignmentPolicy | Where-Object {
+    (Get-ManagementRoleAssignment -RoleAssignee $_.Name).Role -contains "CustomMyDistributionGroups"
+}
+
+Get-Mailbox -ResultSize Unlimited | Where-Object {
+    $_.RoleAssignmentPolicy -eq "RestrictedGroupManagementPolicy"
+} | Select-Object DisplayName, UserPrincipalName
+
+Get-Mailbox -Identity "user@example.com" | Select-Object DisplayName, RoleAssignmentPolicy
+
 # Get current settings of my custom RBAC management role
 Get-ManagementRoleEntry "CustomMyDistributionGroups\*"
 
