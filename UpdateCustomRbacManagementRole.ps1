cde9f024470208b0bd54b11a779d9c43406d7702
Taro KEBI <59550645+tarokebi@users.noreply.github.com>
2025-04-23 13:28:07 +0900

    Update UpdateCustomRbacManagementRole.ps1

--- UpdateCustomRbacManagementRole.ps1
+++ UpdateCustomRbacManagementRole.ps1
@@ -3,10 +3,14 @@ Get-RoleAssignmentPolicy | Where-Object {
     (Get-ManagementRoleAssignment -RoleAssignee $_.Name).Role -contains "CustomMyDistributionGroups"
 }
 
+# List all users who are assigned "Default Role Assignment Policy" (and export to csv)
 Get-Mailbox -ResultSize Unlimited | Where-Object {
-    $_.RoleAssignmentPolicy -eq "RestrictedGroupManagementPolicy"
-} | Select-Object DisplayName, UserPrincipalName
+    $_.RoleAssignmentPolicy -eq "Default Role Assignment Policy"
+} | Select-Object DisplayName, UserPrincipalName |
+Export-Csv -Path "$env:USERPROFILE\_tools\dst\DefaultRoleUsers.csv" -NoTypeInformation -Encoding UTF8
 
+
+# Check specific user's role assignment policy
 Get-Mailbox -Identity "user@example.com" | Select-Object DisplayName, RoleAssignmentPolicy
 
 # Get current settings of my custom RBAC management role
